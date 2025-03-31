69e181ddb04bc58d156dea437e5c11b6caef4742
Diego Velazquez <iseare13@gmail.com>
2025-03-31 00:59:09 -0600

    Prueba desde otra computadora

---
+++ Curso intensivo de Python/Apuntes/04_numeros.py
@@ -0,0 +1,21 @@
+# Números en Python
+entero = 10
+print(entero)
+print(type(entero))
+
+decimal = 10.5
+print(decimal)
+print(type(decimal)) # Muestra el tipo de dato
+
+complejo = 3 + 4j
+print(complejo)
+print(type(complejo))
+
+# Convertir un número decimal a entero
+entero = int(decimal)
+print(entero)
+print(type(entero))
+# Convertir un número entero a decimal
+decimal = float(entero)
+print(decimal)
+print(type(decimal))
