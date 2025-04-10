afa16a7204a2ebb62f1011f6b80ca33378f0b67f
Diego Velazquez <iseare13@gmail.com>
2025-04-10 16:31:57 -0600

    Ultimos apuntes sobre el capitulo 3 sobre listas

--- Curso intensivo de Python/Apuntes/06_listas.py
+++ Curso intensivo de Python/Apuntes/06_listas.py
@@ -50,3 +50,38 @@ print(motorcycles)
 popped_motorcycle = motorcycles.pop(0)  # Elimina el elemento en la posición 1 y lo guarda en una variable
 print(motorcycles)
 print(f'The first motorcycle I owned was a {popped_motorcycle.title()}')
+
+# Eliminar mediante el valor - solo se elimina la primera coincidencia
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')  # Elimina el elemento con el valor 'ducati'
+print(motorcycles)
+
+# Ordenar una lista alfabeticamente 
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print(cars)  
+# Ordenar una lista en orden inverso
+cars.sort(reverse=True)  # Ordena la lista en orden inverso
+print(cars)
+# Ordenar una lista temporalmente
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+print(sorted(cars))  # Ordena la lista temporalmente
+print(cars)  # La lista original no se modifica
+
+# Invertir el orden de los elementos de la lista
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse() 
+print(cars) 
+
+# Obtener la longitud de una lista
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))  # Devuelve la longitud de la lista
+
+# Nota: Las listas generar errores si el indice esta fuera de rango.
+# Nota: Para acceder al último elemento de una lista se puede usar el índice -1, pero si esta vacía generará un error.
+
+
